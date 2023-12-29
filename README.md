29542f1087c5f2e79435ccad66ce47fa2f51a2e2
nvmwhoiam <80307444+nvmwhoiam@users.noreply.github.com>
2023-12-29 03:31:10 +0000

    This is the V2 with a bit of a improvement

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
-# Credit-card-from
- An Credit card form (visually good and functional)
+# Credit Card Form App
+
+## Overview
+
+The **Credit Card Form App** is a simple web application that allows users to enter and validate credit card information. It provides a visually appealing interface to input and view credit card details, including the card number, cardholder name, expiration date, and CVV.
+
+![App Preview](app-preview.png)
+
+The application is built using HTML, CSS, and JavaScript to provide a seamless user experience for entering credit card information securely.
+
+## Features
+
+- **Credit Card Validation**: The app validates credit card numbers using regular expressions to detect card brands such as Visa, MasterCard, American Express, and more.
+
+- **Real-time Formatting**: As the user enters the credit card number, it is formatted in real-time to make it visually appealing and mask sensitive information.
+
+- **Expiration Date Selection**: Users can select the card's expiration month and year from dropdown menus for a convenient input method.
+
+- **CVV Input**: The app supports entering the Card Verification Value (CVV) and displays it as asterisks for security.
+
+- **Card Brand Recognition**: The app dynamically recognizes the card brand based on the entered credit card number and displays the corresponding card logo.
+
+- **Form Submission**: When users submit the form, the app collects the entered information and can be configured to perform actions like sending the data to a server for further processing.
+
+## Installation
+
+To run the Credit Card Form App locally, follow these steps:
+
+1. Clone this repository to your local machine:
+
+git clone https://github.com/nvmwhoiam/Credit-card-from.git
+
+2. Open the `index.html` file in your web browser.
+
+3. You can also serve the app using a local development server if you have one available.
+
+## Usage
+
+1. Open the app in your web browser.
+
+2. Enter the credit card details in the provided input fields:
+
+- **Card Holder**: Enter the name on the credit card.
+- **Card Number**: Enter the 16-digit credit card number.
+- **Expiration Date**: Select the card's expiration month and year.
+- **CVV**: Enter the Card Verification Value (CVV) found on the back of the card.
+
+3. As you enter the card number, the app will automatically format and recognize the card brand.
+
+4. Click the "Save" button to submit the form. You can customize this functionality to suit your needs.
+
+## Customization
+
+You can customize the app's behavior and appearance by modifying the JavaScript code in the `assets/js/index.js` file and the CSS in the `assets/css/index.css` file. Feel free to adapt it to your specific requirements.
+
+## License
+
+This Credit Card Form App is open-source and available under the [MIT License](LICENSE).
+
+## Acknowledgments
+
+- Credit card brand detection and validation are based on commonly used regular expressions found in various online resources.
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,190 +1,141 @@
-window.addEventListener("load", function () {
+// Get the current year
+const year = new Date();
 
-    const year = new Date();
-
-    function cc_brand_id(cur_val) {
-        // the regular expressions check for possible matches as you type, hence the OR operators based on the number of chars
-        // regexp string length {0} provided for soonest detection of beginning of the card numbers this way it could be used for BIN CODE detection also
-
-        //JCB
-        jcb_regex = new RegExp('^(?:2131|1800|35)[0-9]{0,}$'); //2131, 1800, 35 (3528-3589)
+// Function to identify credit card brand based on number
+function cc_brand_id(cur_val) {
+    // Define regular expressions for each card brand
+    const regexes = {
+        // JCB
+        jcb: /^(?:2131|1800|35)[0-9]{0,}$/,
         // American Express
-        amex_regex = new RegExp('^3[47][0-9]{0,}$'); //34, 37
+        amex: /^3[47][0-9]{0,}$/,
         // Diners Club
-        diners_regex = new RegExp('^3(?:0[0-59]{1}|[689])[0-9]{0,}$'); //300-305, 309, 36, 38-39
+        diners: /^3(?:0[0-59]{1}|[689])[0-9]{0,}$/,
         // Visa
-        visa_regex = new RegExp('^4[0-9]{0,}$'); //4
+        visa: /^4[0-9]{0,}$/,
         // MasterCard
-        mastercard_regex = new RegExp('^(5[1-5]|222[1-9]|22[3-9]|2[3-6]|27[01]|2720)[0-9]{0,}$'); //2221-2720, 51-55
-        maestro_regex = new RegExp('^(5[06789]|6)[0-9]{0,}$'); //always growing in the range: 60-69, started with / not something else, but starting 5 must be encoded as mastercard anyway
-        //Discover
-        discover_regex = new RegExp('^(6011|65|64[4-9]|62212[6-9]|6221[3-9]|622[2-8]|6229[01]|62292[0-5])[0-9]{0,}$');
-        ////6011, 622126-622925, 644-649, 65
+        mastercard: /^(5[1-5]|222[1-9]|22[3-9]|2[3-6]|27[01]|2720)[0-9]{0,}$/,
+        // Maestro
+        maestro: /^(5[06789]|6)[0-9]{0,}$/,
+        // Discover
+        discover: /^(6011|65|64[4-9]|62212[6-9]|6221[3-9]|622[2-8]|6229[01]|62292[0-5])[0-9]{0,}$/,
+    };
 
+    // Remove non-digit characters from input
+    cur_val = cur_val.replace(/\D/g, '');
 
-        // get rid of anything but numbers
-        cur_val = cur_val.replace(/\D/g, '');
-
-        // checks per each, as their could be multiple hits
-        //fix: ordering matter in detection, otherwise can give false results in rare cases
-        var sel_brand = "unknown";
-
-        if (cur_val.match(jcb_regex)) {
-            sel_brand = "jcb";
-        } else if (cur_val.match(amex_regex)) {
-            sel_brand = "amex";
-        } else if (cur_val.match(diners_regex)) {
-            sel_brand = "diners_club";
-        } else if (cur_val.match(visa_regex)) {
-            sel_brand = "visa";
-        } else if (cur_val.match(mastercard_regex)) {
-            sel_brand = "mastercard";
-        } else if (cur_val.match(discover_regex)) {
-            sel_brand = "discover";
-        } else if (cur_val.match(maestro_regex)) {
-            if (cur_val[0] == '5') { //started 5 must be mastercard
-                sel_brand = "mastercard";
-            } else {
-                sel_brand = "maestro"; //maestro is all 60-69 which is not something else, thats why this condition in the end
-            }
+    // Iterate through regexes to identify the brand
+    for (const brand in regexes) {
+        if (cur_val.match(regexes[brand])) {
+            return brand;
         }
-
-        return sel_brand;
     }
 
-    const cc_name_show = document.getElementById("cc_name");
-    const cc_numbers_show = document.getElementById("cc_numbers");
-    const cc_month_show = document.getElementById("cc_month");
-    const cc_year_show = document.getElementById("cc_year");
-    const cc_cvv_show = document.getElementById("cc_cvv");
+    return "unknown";
+}
 
-    const cc_name_form = document.querySelector("[name='cc_name']");
-    const cc_numbers_form = document.querySelector("[name='cc_numbers']");
-    const cc_month_form = document.querySelector("[name='cc_month']");
-    const cc_year_form = document.querySelector("[name='cc_year']");
-    const cc_cvv_form = document.querySelector("[name='cc_cvv']");
-    const cc_form = document.getElementById("cc_form");
+// Get DOM elements
+const cardInfoElements = {
+    name: document.getElementById("ccNameCard"),
+    number: document.getElementById("ccNumberCard"),
+    month: document.getElementById("ccMonthCard"),
+    year: document.getElementById("ccYearCard"),
+    cvv: document.getElementById("ccCvvCard"),
+};
 
-    const c_card = document.querySelector(".c_card");
+const formElements = {
+    name: document.querySelector("[name='cc_name']"),
+    numbers: document.querySelector("[name='cc_numbers']"),
+    month: document.querySelector("[name='cc_month']"),
+    year: document.querySelector("[name='cc_year']"),
+    cvv: document.querySelector("[name='cc_cvv']"),
+    form: document.getElementById("cc_form"),
+};
 
-    const currentMonth = year.toLocaleDateString('en', { month: '2-digit' });
-    const currentYear = year.toLocaleDateString('en', { year: '2-digit' });
+const cCard = document.querySelector(".c_card");
 
-    cc_month_show.innerText = currentMonth;
+// Get current month and year
+const currentMonth = year.toLocaleDateString('en', { month: '2-digit' });
+const currentYear = year.toLocaleDateString('en', { year: '2-digit' });
 
-    cc_year_show.innerText = currentYear;
+// Initialize month and year displays
+cardInfoElements.month.innerText = currentMonth;
+cardInfoElements.year.innerText = currentYear;
 
-    cc_name_form.addEventListener("input", function () {
+// Update card name based on input
+formElements.name.addEventListener("input", function () {
+    cardInfoElements.name.innerText = formElements.name.value.length < 1 ? "Mr. John Doe" : formElements.name.value;
+});
 
-        if (cc_name_form.value.length < 1) {
-            cc_name_show.innerText = "Mr. John Doe";
-        } else {
-            cc_name_show.innerText = cc_name_form.value;
+// Update card numbers, masking digits as needed
+formElements.numbers.addEventListener("input", function () {
+    const ccNumbersValue = formElements.numbers.value;
+    const brand = cc_brand_id(ccNumbersValue);
+
+    if (ccNumbersValue.length < 1) {
+        cardInfoElements.number.innerText = "**** **** **** 3221";
+    } else {
+        let maskedNumbers = '';
+        if (brand === "amex") {
+            maskedNumbers = `${ccNumbersValue.substr(0, 4).replace(/[0-9]/g, '*')} ${ccNumbersValue.substr(4, 6).replace(/[0-9]/g, '*')} ${ccNumbersValue.substr(10, 5)}`;
+            formElements.numbers.setAttribute("maxlength", "15");
+        } else if (brand === "visa" || brand === "mastercard") {
+            maskedNumbers = `${ccNumbersValue.substr(0, 4).replace(/[0-9]/g, '*')} ${ccNumbersValue.substr(4, 4).replace(/[0-9]/g, '*')} ${ccNumbersValue.substr(8, 4).replace(/[0-9]/g, '*')} ${ccNumbersValue.substr(12, 4)}`;
+            formElements.numbers.setAttribute("maxlength", "16");
         }
-    });
+        cardInfoElements.number.innerText = maskedNumbers;
+    }
 
-    cc_numbers_form.addEventListener("input", function () {
-
-        const cc_numbers_value = cc_numbers_form.value;
-
-        const mastercardRegex = cc_numbers_value.substr(0, 4).replace(/[0-9]/g, '*') + " " + cc_numbers_value.substr(4, 4).replace(/[0-9]/g, '*') + " " + cc_numbers_value.substr(8, 4).replace(/[0-9]/g, '*') + " " + cc_numbers_value.substr(12, 4);
-
-        const amexRegex = cc_numbers_value.substr(0, 4).replace(/[0-9]/g, '*') + " " + cc_numbers_value.substr(4, 6).replace(/[0-9]/g, '*') + " " + cc_numbers_value.substr(10, 5);
-
-        if (cc_numbers_form.value.length < 1) {
-
-            cc_numbers_show.innerText = "**** **** **** 3221";
-
-        } else {
-
-            if (cc_brand_id(cc_numbers_form.value) === "amex") {
-
-                cc_numbers_form.setAttribute("maxlength", "15");
-                cc_numbers_show.innerText = amexRegex;
-
-            } else if (cc_brand_id(cc_numbers_form.value) === "visa" || cc_brand_id(cc_numbers_form.value) === "mastercard") {
-
-                cc_numbers_form.setAttribute("maxlength", "16");
-                cc_numbers_show.innerText = mastercardRegex;
-
-            }
-
+    const ccBrandImage = document.querySelector(".ccBrand");
+    if (brand === "unknown") {
+        if (ccBrandImage) {
+            ccBrandImage.remove();
         }
-
-
-        if (cc_brand_id(cc_numbers_form.value) === "unknown") {
-            if (document.querySelector(".cc_brand")) {
-                document.querySelector(".cc_brand").remove();
-            }
-        } else {
-            if (!document.querySelector(".cc_brand")) {
-                const img = document.createElement("img");
-                img.classList.add("cc_brand");
-                img.setAttribute("alt", cc_brand_id(cc_numbers_form.value) + " card");
-                document.querySelector(".c_card_front").append(img);
-                document.querySelector(".cc_brand").src = "assets/svgs/" + cc_brand_id(cc_numbers_form.value) + ".svg";
-
-            }
+    } else {
+        if (!ccBrandImage) {
+            const img = document.createElement("img");
+            img.classList.add("ccBrand");
+            img.setAttribute("alt", `${brand} card`);
+            document.querySelector(".c_card_front").append(img);
+            img.src = `assets/svgs/${brand}.svg`;
         }
+    }
+});
 
-    });
+// Update card expiration month
+formElements.month.addEventListener("change", function () {
+    cardInfoElements.month.innerText = formElements.month.value.length < 1 ? currentMonth : formElements.month.value;
+});
 
-    cc_month_form.addEventListener("change", function () {
+// Update card expiration year
+formElements.year.addEventListener("change", function () {
+    cardInfoElements.year.innerText = formElements.year.value.length < 1 ? currentYear : formElements.year.value;
+});
 
-        if (cc_month_form.value.length < 1) {
+// Update card CVV, masking digits as needed
+formElements.cvv.addEventListener("input", function () {
+    cCard.classList.add("back_visible");
+    cardInfoElements.cvv.innerText = formElements.cvv.value.length < 1 ? "***" : formElements.cvv.value.substr(0, 3).replace(/[0-9]/g, '*');
+});
 
-            cc_month_show.innerText = currentMonth;
+// Hide CVV input when blurred
+formElements.cvv.addEventListener("blur", function () {
+    if (cCard.classList.contains("back_visible")) {
+        cCard.classList.remove("back_visible");
+    }
+});
 
-        } else {
+// Form submission handler
+formElements.form.addEventListener("submit", function (e) {
+    e.preventDefault();
 
-            cc_month_show.innerText = cc_month_form.value;
+    const jsonData = {
+        cName: formElements.name.value,
+        cNumbers: formElements.numbers.value,
+        cMonth: formElements.month.value,
+        cYear: formElements.year.value,
+        cCvv: formElements.cvv.value,
+    };
 
-        }
-
-    });
-
-    cc_year_form.addEventListener("change", function () {
-
-        if (cc_year_form.value.length < 1) {
-
-            cc_year_show.innerText = currentYear;
-
-        } else {
-
-            cc_year_show.innerText = cc_year_form.value;
-
-        }
-
-    });
-
-    cc_cvv_form.addEventListener("input", function () {
-
-        c_card.classList.add("back_visible");
-
-        if (cc_cvv_form.value.length < 1) {
-
-            cc_cvv_show.innerText = "***";
-
-        } else {
-
-            cc_cvv_show.innerText = cc_cvv_form.value.substr(0, 3).replace(/[0-9]/g, '*');
-
-        }
-
-    });
-
-    cc_cvv_form.addEventListener("blur", function () {
-        if (c_card.classList.contains("back_visible")) {
-            c_card.classList.remove("back_visible");
-        }
-    })
-
-    cc_form.addEventListener("submit", function (e) {
-        e.preventDefault();
-
-        const requestData = `cc_name=${cc_name_form.value}&cc_numbers=${cc_numbers_form.value}&cc_month=${cc_month_form.value}&cc_year=${cc_year_form.value}&cc_cvv=${cc_cvv_form.value}`;
-
-        console.log(requestData);
-    });
-
+    console.log(jsonData);
 });
